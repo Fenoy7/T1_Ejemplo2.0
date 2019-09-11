14ac297c8c7456bc17fdbb92ec35827e12f56393
Héctor Sánchez Díaz-Maroto <46570590+Fenoy7@users.noreply.github.com>
2019-09-11 18:10:03 +0200

    Update Entrada.java

--- src/Entrada.java
+++ src/Entrada.java
@@ -3,7 +3,7 @@ public class Entrada {
     public static void main(String[] args) {
         System.out.printf("Primer proyecto GitHub");
         System.out.printf("Segunda línea de código");
-        System.out.printf("Les loles");
+        System.out.printf("Pero Fran via web");
     }
 
 }
